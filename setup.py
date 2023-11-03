57fd792b2208de8ec0c08820273844965d673c00
BrandonEH <brandonestradaherrera@gmail.com>
2023-11-03 18:33:53 -0500

    added setup.py and start.ipynb

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='airtraffic_geopolitics',  # This is the package name you choose
+    version='0.1',
+    packages=find_packages(),
+    # Additional metadata about your package
+)
